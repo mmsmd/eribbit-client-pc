13e61dbf16f9fdd4ec572d32355b465a9b204d39
msmmd <mao123488678@gmail.com>
2021-10-19 00:06:44 +0800

    轮播图完成

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -3,10 +3,12 @@
 // vue3.0 插件写法要素：导出一个对象，有install函数，默认传入App应用实例，在app基础上扩展
 
 import XtxSkeleton from './xtx-skeleton.vue'
+import XtxCarousel from './xtx-carousel.vue'
 export default {
   install(app) {
     // 在app上进行扩展，app提供component directive
     // 如果要挂在原型，app.config.globalProperties.$http
     app.component(XtxSkeleton.name, XtxSkeleton)
+    app.component(XtxCarousel.name, XtxCarousel)
   }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="container">
-    <!-- 首页入口 -->
-    <div class="home-entry">
+  <!-- 首页入口 -->
+  <div class="home-entry">
+    <div class="container">
       <!-- 左侧分类 -->
       <HomeCategory></HomeCategory>
       <!-- 轮播图 -->
+      <HomeBanner></HomeBanner>
     </div>
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'HomePage',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   }
 }
 </script>
