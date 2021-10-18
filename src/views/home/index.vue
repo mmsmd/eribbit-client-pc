15f24f478decbb1dfee417429e03a908c7222e69
msmmd <mao123488678@gmail.com>
2021-10-18 14:31:15 +0800

    左侧分类-结构渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="container">首页内容</div>
+  <div class="container">
+    <!-- 首页入口 -->
+    <div class="home-entry">
+      <!-- 左侧分类 -->
+      <HomeCategory></HomeCategory>
+      <!-- 轮播图 -->
+    </div>
+  </div>
 </template>
 
 <script>
+import HomeCategory from './components/home-category.vue'
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    HomeCategory
+  }
 }
 </script>
 
