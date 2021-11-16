316521f2c9397541d111b1b5ed853a4f55e8a0c5
msmmd <mao123488678@gmail.com>
2021-11-16 22:42:21 +0800

    商品详情-同类推荐组件

--- src/api/product.js
+++ src/api/product.js
@@ -8,3 +8,12 @@ import request from '@/utils/request.js'
 export const findGoods = id => {
   return request('/goods', 'get', { id })
 }
+
+/**
+ * 获取 相关推荐 | 猜你喜欢 商品
+ * @param {String} id - 商品ID，传入-相关推荐，不传-猜你喜欢
+ * @returns {Integer} limit - 商品数量
+ */
+export const findRelevantGoods = ({ id, limit = 16 }) => {
+  return request('/goods/relevant', 'get', { id, limit })
+}
