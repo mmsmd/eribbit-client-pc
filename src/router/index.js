513ce2e1dc5ce27f77fb312c805a3958aa1027f6
msmmd <mao123488678@gmail.com>
2021-11-19 22:03:47 +0800

    登录-基础布局

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ const TopCategory = () => import('@/views/category/index.vue')
 const SubCategory = () => import('@/views/category/sub.vue')
 const Goods = () => import('@/views/goods/index.vue')
 
+const Login = () => import('@/views/login/index.vue')
+
 const routes = [
   //  一级路由布局容器
   {
@@ -17,7 +19,8 @@ const routes = [
       { path: '/category/sub/:id', component: SubCategory },
       { path: '/product/:id', component: Goods }
     ]
-  }
+  },
+  { path: '/login', component: Login }
 ]
 
 // Vue2.0 new VueRouter({}) 创建路由实例
