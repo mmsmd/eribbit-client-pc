4a8f2fd7a7a8075a1a047a1d096e9b90bffe7044
msmmd <mao123488678@gmail.com>
2021-11-24 19:22:17 +0800

    登录-退出登录

--- src/components/app-navbar.vue
+++ src/components/app-navbar.vue
@@ -6,7 +6,7 @@
           <li>
             <a href="javascript:;"><i class="iconfont icon-user"></i>{{ profile.account }}</a>
           </li>
-          <li><a href="javascript:;">退出登录</a></li>
+          <li><a @click="logout()" href="javascript:;">退出登录</a></li>
         </template>
         <template v-else>
           <li><RouterLink to="/login">请先登录</RouterLink></li>
@@ -27,6 +27,7 @@
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 export default {
   name: 'AppTopnav',
   setup() {
@@ -37,7 +38,16 @@ export default {
       return store.state.user.profile
     })
 
-    return { profile }
+    // 退出登录
+    // 1.清空本地存储信息和vuex的用户信息
+    // 2.跳转登录
+    const router = useRouter()
+    const logout = () => {
+      store.commit('user/setUser', {})
+      router.push('/login')
+    }
+
+    return { profile, logout }
   }
 }
 </script>
