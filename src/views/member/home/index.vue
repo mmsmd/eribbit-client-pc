78af4e708b6b302288b9f5d0d1f8b9e864572c70
msmmd <mao123488678@gmail.com>
2021-12-09 21:58:23 +0800

    个人中心-渲染页面

--- src/views/member/home/index.vue
+++ src/views/member/home/index.vue
@@ -3,9 +3,13 @@
     <!-- 概览 -->
     <HomeOverview></HomeOverview>
     <!-- 收藏 -->
-    <HomePanel title="我的收藏"></HomePanel>
+    <HomePanel title="我的收藏">
+      <GoodItem v-for="i in 4" :key="i" :goods="goods"></GoodItem>
+    </HomePanel>
     <!-- 组件 -->
-    <HomePanel title="我的足迹"></HomePanel>
+    <HomePanel title="我的足迹">
+      <GoodItem v-for="i in 4" :key="i" :goods="goods"></GoodItem>
+    </HomePanel>
     <!-- 猜你喜欢 -->
     <GoodsRelevant></GoodsRelevant>
   </div>
@@ -15,12 +19,24 @@
 import HomeOverview from './components/home-overview.vue'
 import HomePanel from './components/home-panel.vue'
 import GoodsRelevant from '@/views/goods/components/goods-relevant.vue'
+import GoodItem from '@/views/category/components/goods-item.vue'
 export default {
   name: 'MemberHome',
   components: {
     HomeOverview,
     HomePanel,
-    GoodsRelevant
+    GoodsRelevant,
+    GoodItem
+  },
+  setup() {
+    const goods = {
+      id: '1',
+      name: '自煮火锅不排队 麦饭石不粘鸳鸯火锅',
+      picture: 'https://yanxuan-item.nosdn.127.net/fcdcb840a0f5dd754bb8fd2157579012.jpg',
+      desc: '清汤鲜香 红汤劲爽',
+      price: '159.00'
+    }
+    return { goods }
   }
 }
 </script>
