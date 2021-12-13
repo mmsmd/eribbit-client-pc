390a03fcc8d5dc43323721bc4b90daaafcf1395c
msmmd <mao123488678@gmail.com>
2021-12-13 20:43:18 +0800

    订单管理-列表渲染

--- src/api/order.js
+++ src/api/order.js
@@ -45,3 +45,14 @@ export const submitOrder = params => {
 export const findOrderDetail = orderId => {
   return request('/member/order/' + orderId, 'get')
 }
+
+/**
+ * 获取订单列表
+ * @param {Number} page - 页码
+ * @param {Number} pageSize - 每页条数
+ * @param {Number} orderState - 订单状态，1为待付款、2为待发货、3为待收货、4为待评价、5为已完成、6为已取消，未传该参数或0为全部
+ * @returns
+ */
+export const findOrderList = ({ page = 1, pageSize = 10, orderState = 0 }) => {
+  return request('/member/order/', 'get', { page, pageSize, orderState })
+}
--- src/views/member/order/index.vue
+++ src/views/member/order/index.vue
@@ -1,27 +1,60 @@
 <template>
   <div class="member-order">
-    <!-- 体验jsx语法 -->
-    <XtxTabs v-model="activeName" @tab-click="changeTab">
-      <XtxTabsPanel label="选项卡0" name="first">内容0 </XtxTabsPanel>
-      <XtxTabsPanel v-for="i in 4" :key="i" :label="`选项卡${i}`" :name="`name${i}`"
-        >内容{{ i }}</XtxTabsPanel
+    <!-- tab组件 -->
+    <XtxTabs v-model="activeName">
+      <XtxTabsPanel
+        v-for="item in orderStatus"
+        :key="item.name"
+        :label="item.label"
+        :name="item.name"
       >
+      </XtxTabsPanel>
     </XtxTabs>
+    <!-- 订单列表 -->
+    <div class="order-list">
+      <OrderItem v-for="item in orderList" :key="item.id" :order="item"></OrderItem>
+    </div>
+    <!-- 分页组件 -->
+    <XtxPagination></XtxPagination>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+import { orderStatus } from '@/api/constance.js'
+import OrderItem from '@/views/member/order/components/order-item.vue'
+import { findOrderList } from '@/api/order.js'
 export default {
   name: 'MemberOrder',
+  components: {
+    OrderItem
+  },
   setup() {
-    const activeName = ref('first')
-    const changeTab = tab => {
-      console.log(tab)
-    }
-    return { activeName, changeTab }
+    const activeName = ref('all')
+
+    // 获取数据
+    const reqParams = reactive({
+      page: 1,
+      pageSize: 10,
+      orderState: 0
+    })
+    const orderList = ref([])
+    findOrderList(reqParams).then(data => {
+      orderList.value = data.result.items
+    })
+
+    return { activeName, orderStatus, orderList }
   }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.member-order {
+  height: 100%;
+  background: #fff;
+}
+.order-list {
+  background: #fff;
+  padding: 20px;
+}
+</style>
