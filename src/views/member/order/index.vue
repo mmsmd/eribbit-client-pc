da213ee23cea5837aac6803fbb0b41eb08d14077
msmmd <mao123488678@gmail.com>
2021-12-14 21:53:38 +0800

    订单管理-条件查询

--- src/views/member/order/index.vue
+++ src/views/member/order/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="member-order">
     <!-- tab组件 -->
-    <XtxTabs v-model="activeName">
+    <XtxTabs v-model="activeName" @tab-click="tabClick">
       <XtxTabsPanel
         v-for="item in orderStatus"
         :key="item.name"
@@ -12,15 +12,23 @@
     </XtxTabs>
     <!-- 订单列表 -->
     <div class="order-list">
+      <div v-if="loading" class="loading"></div>
+      <div class="none" v-if="!loading && orderList.length === 0">暂无数据</div>
       <OrderItem v-for="item in orderList" :key="item.id" :order="item"></OrderItem>
     </div>
     <!-- 分页组件 -->
-    <XtxPagination></XtxPagination>
+    <XtxPagination
+      v-if="total > 0"
+      :current-page="reqParams.page"
+      :page-size="reqParams.pageSize"
+      :total="total"
+      @current-change="reqParams.page = $event"
+    ></XtxPagination>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import { orderStatus } from '@/api/constance.js'
 import OrderItem from '@/views/member/order/components/order-item.vue'
 import { findOrderList } from '@/api/order.js'
@@ -32,18 +40,37 @@ export default {
   setup() {
     const activeName = ref('all')
 
-    // 获取数据
+    // 筛选条件
     const reqParams = reactive({
       page: 1,
-      pageSize: 10,
+      pageSize: 5,
       orderState: 0
     })
     const orderList = ref([])
-    findOrderList(reqParams).then(data => {
-      orderList.value = data.result.items
-    })
+    const loading = ref(false)
+    const total = ref(0)
 
-    return { activeName, orderStatus, orderList }
+    // 筛选条件变化重新加载
+    watch(
+      reqParams,
+      () => {
+        loading.value = true
+        findOrderList(reqParams).then(data => {
+          orderList.value = data.result.items
+          total.value = data.result.counts
+          loading.value = false
+        })
+      },
+      { immediate: true }
+    )
+
+    // 点击选项卡
+    const tabClick = ({ index }) => {
+      reqParams.page = 1
+      reqParams.orderState = index
+    }
+
+    return { activeName, orderStatus, orderList, tabClick, loading, total, reqParams }
   }
 }
 </script>
@@ -56,5 +83,21 @@ export default {
 .order-list {
   background: #fff;
   padding: 20px;
+  position: relative;
+  min-height: 400px;
+}
+.loading {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: rgba(255, 255, 255, 0.9) url(../../../assets/images/loading.gif) no-repeat center;
+}
+.none {
+  height: 400px;
+  text-align: center;
+  line-height: 400px;
+  color: #999;
 }
 </style>
