bc9bf245d74136dfbdf84653a617310327f4cf2c
msmmd <mao123488678@gmail.com>
2021-12-16 21:00:35 +0800

    订单详情-头部展示

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="member-detail">MemberDetail</div>
+  <div class="member-order-detail" v-if="order">
+    <!-- 头部 -->
+    <DetailAction :order="order"></DetailAction>
+    <!-- 进度 -->
+    <!-- 物流 -->
+    <!-- 商品订单信息 -->
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import DetailAction from './components/detail-action.vue'
+import { findOrderDetail } from '@/api/order.js'
 export default {
-  name: 'MemberDetail'
+  name: 'MemberDetail',
+  components: {
+    DetailAction
+  },
+  setup() {
+    const route = useRoute()
+
+    const order = ref(null)
+    findOrderDetail(route.params.id).then(data => {
+      order.value = data.result
+    })
+
+    return { order }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.member-order-detail {
+  background: #fff;
+  height: 100%;
+}
+</style>
